1d4e43ae8013829d5fed51425c0d59f947b50773
Sharan Varma <sharan.varma@skit.ai>
2025-10-01 22:40:17 +0530

    Improve: processing: Add main function

--- process_emails.py
+++ process_emails.py
@@ -3,13 +3,19 @@ from utils import parsing_utils
 from utils.rule_parser import RuleParser
 from utils.dbutils import db_funcs
 from utils.dbutils import emailbase
+import os
 
-if __name__ == '__main__':
+project_dir = os.getenv("PYTHONPATH")
+test = os.getenv("TEST", False)
+
+def main():
     email_session = emailbase.EmailBase()
+    if test:
+        email_session = emailbase.EmailBaseTest()
     email_db_engine = email_session.connect_to_database()
     email_session.initialize_db_operations()
 
-    config_file = "rules.json"
+    config_file = f"{project_dir}/rules.json"
 
     json_content = None
     with open(config_file, 'r') as file:
@@ -19,3 +25,7 @@ if __name__ == '__main__':
 
     for statement, kwargs in sql_statements:
         db_funcs.execute_raw_sql_query(email_session.database_engine, statement, kwargs)
+
+
+if __name__ == '__main__':
+    main()
