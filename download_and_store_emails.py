a0d51306f382d09fae49b54f316ded6a3817f66b
Sharan Varma <sharan.varma@skit.ai>
2025-10-01 17:55:22 +0530

    2 Main scripts: Downloading and Processing

--- download_and_store_emails.py
+++ download_and_store_emails.py
@@ -1,12 +1,12 @@
 from loguru import logger
-from utils import parsing_utils
 from utils.emailutils import gmail
 from utils.dbutils import emailbase
-from utils.emailutils import email
+from utils import parsing_utils
 
 
 if __name__ == "__main__":
-    db_connection = emailbase.connect_to_database()
+    email_base_obj = emailbase.EmailBase()
+    db_connection = email_base_obj.connect_to_database()
     gmail_credentials = gmail.login_to_gmail()
     service_obj = gmail.initialize_gmail_interaction(gmail_credentials)
     list_of_emails = parsing_utils.get_list_of_emails(service_obj)
@@ -17,12 +17,5 @@ if __name__ == "__main__":
         email_database_record = emailbase.EmailBase.from_email(email)
         logger.debug(f"Converted Email to DB record")
         list_of_db_records.append(email_database_record)
-
-    logger.debug(f"Number of records processed: {len(list_of_db_records)}")
-
-    email_session = emailbase.EmailBase()
-    emailbase.initialize_db_operations(db_connection)
-    
-    emailbase.insert_records_into_db(db_connection, list_of_db_records)
-    
-
+    logger.debug(f"Number of records downloaded: {len(list_of_db_records)}")
+    email_base_obj.insert_records_into_db(list_of_db_records)
---
+++ process_emails.py
@@ -0,0 +1,21 @@
+from loguru import logger
+from utils import parsing_utils
+from utils.rule_parser import RuleParser
+from utils.dbutils import db_funcs
+from utils.dbutils import emailbase
+
+if __name__ == '__main__':
+    email_session = emailbase.EmailBase()
+    email_db_engine = email_session.connect_to_database()
+    email_session.initialize_db_operations()
+
+    config_file = "rules.json"
+
+    json_content = None
+    with open(config_file, 'r') as file:
+        json_content = file.read()
+    rule_parser = RuleParser(json_content)
+    sql_statements = rule_parser.parse_operations()
+
+    for statement, kwargs in sql_statements:
+        db_funcs.execute_raw_sql_query(email_session.database_engine, statement, kwargs)
