4ef64167f3ab140244ccb9e95853919d62d9507a
Sharan Varma <sharan.varma@skit.ai>
2025-10-01 17:56:59 +0530

    Add: Util functions for parsing gmail info

--- utils/parsing_utils.py
+++ utils/parsing_utils.py
@@ -10,19 +10,43 @@ def parse_received_datetime(received_entry_raw):
         date_str = received_entry_raw
         if ';' in received_entry_raw:
             date_str = received_entry_raw.split(';')[1]
-        parsed_datetime = parsedate_to_datetime(date_str)
+        parsed_date = parsedate_to_datetime(date_str).date()
         logger.debug(f"Parsed DateTime: {parsed_datetime}")
     except Exception as date_time_parsing_exc:
         logger.error(f"Exception when attempting to parse datetime in received header: {date_time_parsing_exc}")
-    return parsed_datetime
+    return parsed_date.isoformat()
         
 
 def get_header(name, headers):
     return next((h['value'] for h in headers if h['name'].lower() == name.lower()), None)
 
+def get_mailbox_and_read_status(labels):
+    valid_mailboxes = ['INBOX', 'IMPORTANT', 'CATEGORY_PROMOTIONS']
+    valid_read_status = ['READ', 'UNREAD']
+    mailbox, is_read = None, None
+
+    for mailbox_iter in valid_mailboxes:
+        if mailbox_iter in labels:
+            mailbox = mailbox_iter 
+            break
+
+    for read_status_iter in valid_read_status:
+        if read_status_iter in labels:
+            is_read = read_status_iter
+            break
+    return mailbox, is_read
+
+
 def parse_email(raw_email_item, headers):
     parsed_email = {}
     try:
+        label_ids = raw_email_item.get("labelIds", [])
+        mailbox, is_read = get_mailbox_and_read_status(label_ids)
+        logger.debug(f"{mailbox}, {is_read}")
+        if is_read == 'UNREAD':
+            is_read = False
+        else:
+            is_read = True
         parsed_email["sender"] = get_header("From", headers)
         parsed_email["receiver"] = get_header("To", headers)
         parsed_email["sent_timestamp"] = parse_received_datetime(get_header("Date", headers))
@@ -30,6 +54,8 @@ def parse_email(raw_email_item, headers):
         parsed_email["subject"] = get_header("Subject", headers)
         parsed_email["cc"] = get_header("Cc", headers)
         parsed_email["bcc"] = get_header("Bcc", headers)
+        parsed_email["mailbox"] = mailbox
+        parsed_email["is_read"] = is_read
         logger.debug(f"Parsed Email: {parsed_email}")
     except Exception as exc:
         logger.error(f"Exception when parsing email: {exc}")
@@ -56,4 +82,3 @@ def get_list_of_emails(service_obj, number_of_results=10):
             logger.error(f"Error when describing emails by id: {exc}")
     logger.debug(f"Messages obtained: {len(parsed_messages)}")
     return parsed_messages
-
