9fc79e555c647ba13c259e8b1015959897fce2ec
Sharan Varma <sharan.varma@skit.ai>
2025-10-07 22:35:56 +0530

    Add: gmail login + direct gmail inbox update

--- process_emails.py
+++ process_emails.py
@@ -3,6 +3,7 @@ from utils import parsing_utils
 from utils.rule_parser import RuleParser
 from utils.dbutils import db_funcs
 from utils.dbutils import emailbase
+from utils.emailutils import gmail
 import os
 
 project_dir = os.getenv("PYTHONPATH")
@@ -10,6 +11,8 @@ test = os.getenv("TEST", False)
 
 def main():
     email_session = emailbase.EmailBase()
+    gmail_credentials = gmail.login_to_gmail()
+    service_obj = gmail.initialize_gmail_interaction(gmail_credentials)
     if test:
         email_session = emailbase.EmailBaseTest()
     email_db_engine = email_session.connect_to_database()
@@ -21,7 +24,17 @@ def main():
     with open(config_file, 'r') as file:
         json_content = file.read()
     rule_parser = RuleParser(json_content)
-    sql_statements = rule_parser.parse_operations()
+    sql_statements, select_statements = rule_parser.parse_operations(test=test)
+    
+    email_ids = []
+
+    for select_statement, kwargs, target_labels in select_statements:
+        result = db_funcs.execute_raw_sql_query_and_return_result(email_session.database_engine, select_statement, kwargs)
+        for row in result:
+            email_ids.append(row[0])
+        for mail_id in email_ids:
+            gmail.update_email(service_obj, mail_id, target_labels)
+            
 
     for statement, kwargs in sql_statements:
         db_funcs.execute_raw_sql_query(email_session.database_engine, statement, kwargs)
