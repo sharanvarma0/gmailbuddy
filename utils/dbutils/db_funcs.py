1540746cb6cb5505641e4373eb7036d43e9c33d4
Sharan Varma <sharan.varma@skit.ai>
2025-10-07 23:19:53 +0530

    Add: func to get select statement outputs

--- utils/dbutils/db_funcs.py
+++ utils/dbutils/db_funcs.py
@@ -10,3 +10,13 @@ def execute_raw_sql_query(db_engine, query, query_kwargs):
             conn.commit()
     except Exception as sql_query_exec_exception:
         logger.error(f"Exception when attempting to execute raw sql query: {sql_query_exec_exception}")
+
+
+def execute_raw_sql_query_and_return_result(db_engine, query, query_kwargs):
+    try:
+        logger.debug(f"Received Query: {query}")
+        with Session(db_engine) as conn:
+            result = conn.execute(text(query), {**query_kwargs})
+            return result
+    except Exception as sql_query_exec_exception:
+        logger.error(f"Exception when attempting to execute raw sql query: {sql_query_exec_exception}")
